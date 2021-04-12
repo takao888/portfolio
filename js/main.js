918a7f92fd0861a3a2e495584697f9496c0e5754
takao888 <75712160+takao888@users.noreply.github.com>
2021-04-12 19:14:00 +0900

    追加

--- js/main.js
+++ js/main.js
@@ -18,60 +18,103 @@ jQuery(document).ready(function( $ ) {
     $(".navbar-collapse.collapse").removeClass('in');
   });
 
-  // Javascript Chart
-		if($('#javascript').length) {
+  // html/css Chart
+		if($('#htmlcss').length) {
 
 			var doughnutData = [{
-	        value: 70,
+	        value: 50,
 	        color: "#74cfae"
 	      },
 	      {
-	        value: 30,
+	        value: 50,
 	        color: "#3c3c3c"
 	      }
 	    ];
-	    var myDoughnut = new Chart(document.getElementById("javascript").getContext("2d")).Doughnut(doughnutData);
+	    var myDoughnut = new Chart(document.getElementById("htmlcss").getContext("2d")).Doughnut(doughnutData);
 		};
 
-		// Bootstrap Chart
-		if($('#bootstrap').length) {
+		// Javascript Chart
+		if($('#javascript').length) {
 			var doughnutData = [{
-					value: 90,
-					color: "#74cfae"
-				},
-				{
-					value: 10,
-					color: "#3c3c3c"
-				}
-			];
-			var myDoughnut = new Chart(document.getElementById("bootstrap").getContext("2d")).Doughnut(doughnutData);
-		}
-
-		// WordPress Chart
-		if($('#wordpress').length) {
-			var doughnutData = [{
-					value: 55,
-					color: "#74cfae"
-				},
-				{
-					value: 45,
-					color: "#3c3c3c"
-				}
-			];
-			var myDoughnut = new Chart(document.getElementById("wordpress").getContext("2d")).Doughnut(doughnutData);
-		}
-
-		// Photoshop Chart
-		if($('#photoshop').length) {
-			var doughnutData = [{
-					value: 80,
-					color: "#74cfae"
-				},
-				{
 					value: 20,
+					color: "#74cfae"
+				},
+				{
+					value: 80,
 					color: "#3c3c3c"
 				}
 			];
-			var myDoughnut = new Chart(document.getElementById("photoshop").getContext("2d")).Doughnut(doughnutData);
+			var myDoughnut = new Chart(document.getElementById("javascript").getContext("2d")).Doughnut(doughnutData);
 		}
-});
+
+		// Ruby Chart
+		if($('#ruby').length) {
+			var doughnutData = [{
+					value: 50,
+					color: "#74cfae"
+				},
+				{
+					value: 50,
+					color: "#3c3c3c"
+				}
+			];
+			var myDoughnut = new Chart(document.getElementById("ruby").getContext("2d")).Doughnut(doughnutData);
+		}
+
+		// Ruby on rails Chart
+		if($('#rubyonrails').length) {
+			var doughnutData = [{
+					value: 40,
+					color: "#74cfae"
+				},
+				{
+					value: 60,
+					color: "#3c3c3c"
+				}
+			];
+			var myDoughnut = new Chart(document.getElementById("rubyonrails").getContext("2d")).Doughnut(doughnutData);
+		}
+
+		// tule Chart
+		if($('#tule').length) {
+			var doughnutData = [{
+					value: 40,
+					color: "#74cfae"
+				},
+				{
+					value: 60,
+					color: "#3c3c3c"
+				}
+			];
+			var myDoughnut = new Chart(document.getElementById("tule").getContext("2d")).Doughnut(doughnutData);
+		}
+
+				// test Chart
+				if($('#test').length) {
+					var doughnutData = [{
+							value: 30,
+							color: "#74cfae"
+						},
+						{
+							value: 70,
+							color: "#3c3c3c"
+						}
+					];
+					var myDoughnut = new Chart(document.getElementById("test").getContext("2d")).Doughnut(doughnutData);
+				}
+		
+				// db Chart
+		if($('#db').length) {
+			var doughnutData = [{
+					value: 30,
+					color: "#74cfae"
+				},
+				{
+					value: 70,
+					color: "#3c3c3c"
+				}
+			];
+			var myDoughnut = new Chart(document.getElementById("db").getContext("2d")).Doughnut(doughnutData);
+		}
+
+	});
